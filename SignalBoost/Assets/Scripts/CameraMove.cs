a748e7c27ffde08380998fb9a9b3c6d5ec9943a0
Naamed <57773739+Naamed@users.noreply.github.com>
2019-11-16 23:41:15 +0200

    more complete push of signalboost
    All assets of signalboost excluding a spawner script that doesn't compile

---
+++ SignalBoost/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField]
+    public float sensitivity = 5.0f;
+    [SerializeField]
+    public float smoothing = 2.0f;
+    public GameObject player;
+    private Vector2 mouseLook;
+    private Vector2 smoothV;
+
+    // Start is called before the first frame update
+    void Start() => player = transform.parent.gameObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        // Kahden floatin arvojen lisäys ja tulos
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        // Joka lisätään pienissä osissa kameran ohjaukseen
+        mouseLook += smoothV;
+
+        // vector3.right = x-axis
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, player.transform.up);
+    }
+}
---
+++ SignalBoost/Assets/Scripts/TimedRemoval.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedRemoval : MonoBehaviour
+{
+    void DestroyObjectDelayed()
+    {
+        // Kills the game object in X seconds after loading the object
+        Destroy(gameObject, 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //Same basic logic can be used here, just with additional inputs before triggering the deletion 
+    }
+}
