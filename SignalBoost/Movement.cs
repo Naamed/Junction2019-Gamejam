a9145ab51fa316f4d104efbc1d5a10b0d9f2f484
Naamed <57773739+Naamed@users.noreply.github.com>
2019-11-16 05:05:19 +0200

    SignalBoost's scripts
    A basic unity manual consulted movement and camera control script, useable outside of FPS perspective with few setting adjustments

---
+++ SignalBoost/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private float translation;
+    private float strafe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        strafe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(strafe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
