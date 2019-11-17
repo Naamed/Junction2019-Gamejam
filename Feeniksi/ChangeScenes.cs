b33676d7d9878e3cea7684b51264dd22310e08f5
Naamed <57773739+Naamed@users.noreply.github.com>
2019-11-17 06:43:27 +0200

    Boom, update time
    Final changes to notable phoenix scripts as well as readme details for the phoenix project, detailing where to obtain the bloated project file that was too big for git 2 legit 2 quit.

---
+++ Feeniksi/ChangeScenes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeScenes : MonoBehaviour
+{
+    public string levelToLoad;
+    private float timer = 10f;
+    private Text timerSeconds;
+    void Start()
+    {
+        timerSeconds = GetComponent<Text>();
+    }
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f2");
+        if (timer <= 0)
+        {
+            Application.LoadLevel(levelToLoad);
+        }
+    }
+}
--- Feeniksi/readme.txt
+++ Feeniksi/readme.txt
@@ -1,1 +1,4 @@
-<>
+Here are the more important scripts of this project.
+
+For the ENTIRE project, refer to this download link:
+https://drive.google.com/open?id=1PBbKYjc4bi2i4Mh2fdB91H0SIByGrdyM
